681a4dadad374fef35849952ef94dc5d9ec787ef
huangdii <hwangdi66@gmail.com>
2021-10-07 20:34:33 +0000

    HW4

---
+++ rottenpotatoes/spec/factories/movie_factory.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :movie do
+    title 'No name'
+    rating 'G'
+    description 'No description'
+    release_date '2021-10-06'
+  end
+end
--- rottenpotatoes/spec/rails_helper.rb
+++ rottenpotatoes/spec/rails_helper.rb
@@ -1,3 +1,4 @@
+require 'rails_helper'
 require 'simplecov'
 SimpleCov.start 'rails'
 
@@ -9,6 +10,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
